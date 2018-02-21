1b88918fec4c2df8b270ce1e2d52b3d1b76df413
Tran Van Thuc <thuc.tv@workhouse.me>
2018-02-21 10:13:56 +0700

    Setup and hide css files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NoMatch from './components/NoMatch';
 import Register from './components/Register';
+import Test from './components/Test';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div>
         <Switch>
           <Route exact path="/" component={Register} />
+          <Route path="/test" component={Test} />
           <Route component={NoMatch} />
         </Switch>
       </div>
---
+++ src/components/Test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'styles/test.css';
+
+const Test = () => {
+  return (
+    <div className="container">
+      <div className="item1">1</div>
+      <div className="item2">2</div>
+      <div className="item3">3</div>
+      <div className="item4">4</div>
+      <div className="item5">5</div>
+      <div className="item6">6</div>
+    </div>
+  );
+};
+
+export default Test;
